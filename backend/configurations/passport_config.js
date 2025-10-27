a7f2f6e5bad922ab7750f5bcb3f659fae1900c9d
Hara prasad mahapatra <haraprasadmahapatra223@gmail.com>
2025-10-27 15:49:44 +0530

    call back url resolved

--- backend/configurations/passport_config.js
+++ backend/configurations/passport_config.js
@@ -41,7 +41,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://sangam-l1oo.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
