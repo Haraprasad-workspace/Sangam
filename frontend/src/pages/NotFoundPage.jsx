34b1a70a8cb68ce445c968e11fecfbf7027ab3d8
Hara prasad mahapatra <haraprasadmahapatra223@gmail.com>
2026-03-08 14:09:35 +0530

    UI change in not found page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const NotFoundPage = () => {
   return (
     <div className='bg-cover bg-center font-oswald bg-parchment  w-full min-h-screen flex flex-col justify-between'>
-        <div className='text-3xl font-bold m-auto font-Montserrat p-4'>Error:- 404 Page Not found </div>
+        <p className="text-center text-gray-700">
+            Page not found. <br />
+            Please log out and log in again to restart your session. <br />
+            If the issue persists, please contact: <b>haraprasadmahapatra223@gmail.com</b>
+        </p>
+        
     </div>
   )
 }
